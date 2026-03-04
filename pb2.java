c36493775573fbdd6e28e1a2738a14302d136615
Pravin-std <pravinstark456@gmail.com>
2026-03-04 13:08:57 +0530

    seat

--- pb2.java
+++ pb2.java
@@ -1,20 +1,19 @@
 package NestedForLoop;
 import java.util.*;
-public class pb1
-{
+public class pb2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-
+        System.out.println("Seating Arrangement: ");
         for(int row = 1;row<=n1;row++)
         {
-            for(int col = 1;col <=n2;col++)
+            System.out.print("Row "+ row  +":");
+            for(int col = 1;col<=n2;col++)
             {
-                System.out.print(col*row+ " ");
+                System.out.print("Seat-"+col+" ");
             }
             System.out.println();
         }
     }
-    
 }
